87ec08195a79f00f278a6afa493b0f17261b98f6
thePeter876 <38359059+thePeter876@users.noreply.github.com>
2019-11-06 11:50:03 +0100

    luz puntual

--- PhaserBase/gameTest.js
+++ PhaserBase/gameTest.js
@@ -1,7 +1,7 @@
 var config = {
         type: Phaser.AUTO,
         width: 800,
-        height: 600,
+        height: 800,
         physics: {
             default: 'arcade',
             arcade: {
@@ -16,6 +16,10 @@ var config = {
 
     var game = new Phaser.Game(config);
 
+    var pipeline_light;
+    var pipeline_surface;
+    //var rt;
+
     function preload ()
     {
 
@@ -24,7 +28,13 @@ var config = {
         //this.load.setBaseURL('http://labs.phaser.io');
 
         //this.load.image('sky', 'assets/skies/space3.png');
-        this.load.image('logo', ['./assets/test/21749-5-yoshi-file.png' , './assets/test/normalMap.png']);
+        this.load.image('yoshi', ['./assets/test/21749-5-yoshi-file.png' , './assets/test/normalMap.png']);
+        this.load.image('mario', ['./assets/test/mario.png', './assets/test/normalMap.png']);
+
+
+
+        //this.game.load.shader('invert', 'assets/shaders/invert.frag');
+
         //this.load.image('red', 'assets/particles/red.png');
     }
 
@@ -33,12 +43,25 @@ var config = {
         //this.lights.enable().setAmbientColor(0x111111);
         //console.log(this.lights.getMaxVisibleLights());
 
-        this.add
-          .image(360, 300, 'logo')
+
+
+        var spriteYoshi = this.add
+          .sprite(300, 300, 'yoshi')
           .setOrigin(0.5)
           .setScale(0.5)
+
           //.setPipeline("Light2D")
         ;
+
+        var spriteMario = this.add
+          .sprite(600, 300, 'mario')
+          .setOrigin(0.5)
+          .setScale(0.2)
+
+          //.setPipeline("Light2D")
+        ;
+        //spriteYoshi.blendMode = Phaser.BlendModes.ADD;
+        //spriteMario.blendMode = Phaser.BlendModes.ADD;
 
         //var light = this.lights.addLight(400, 300, 400).setIntensity(0.9);
 
@@ -48,8 +71,52 @@ var config = {
         });*/
 
 
-        this.grayscalePipeline = this.game.renderer.addPipeline('Grayscale', new GrayscalePipeline(this.game));
-        this.cameras.main.setRenderToTexture(this.grayscalePipeline);
+        //this.grayscalePipeline = this.game.renderer.addPipeline('Grayscale', new GrayscalePipeline(this.game));
+        //this.invertPipeline = this.game.renderer.addPipeline('Invert', new InvertPipeline(this.game));
+
+        //this.game.renderer.addPipeline('Surface', new SurfacePipeline(this.game));
+        //this.game.renderer.addPipeline('Surface1', new SurfacePipeline(this.game));
+
+
+        //spriteYoshi.setPipeline('Surface');
+        //spriteYoshi.pipeline.setFloat1('lightTransfer', 1.0);
+        //spriteMario.setPipeline('Surface1');
+        //spriteMario.pipeline.setFloat1('lightTransfer', 1.0);
+
+        //LIGHT PIPELINE
+        pipeline_light = this.game.renderer.addPipeline('Lighting', new LightingPipeline(this.game));
+        pipeline_light.setFloat2('res', 100, 100);
+        pipeline_light.setFloat2('light1_Pos', 0.5, 0.5);
+
+        //SURFACE PIPELINE
+        pipeline_surface = this.game.renderer.addPipeline('Surface', new SurfacePipeline(this.game));
+        pipeline_surface.setFloat1('lightTransfer', 1.0);
+
+
+        //CAMERAS
+        var cam1 = this.cameras.main;
+        cam1.setRenderToTexture(/*pipeline_light*/pipeline_light);
+
+
+        //texture2D a = pipeline_light.texture2D;
+
+        //LIGHT POSITION
+        this.input.on('pointermove', function(pointer){
+          pipeline_light.setFloat2('light1_Pos', pointer.x/800.0, pointer.y/800.0);
+        });
+        //sprite.setPipeline('Invert');
+
+
+        //var cam2 = this.cameras.add(0,0,800,600);
+
+
+
+        //cam2.setRenderToTexture(this.invertPipeline);
+
+        //cam2.setRenderToTexture(this.invertPipeline);
+        //cam2.setPipeline('Invert');
+
+        //this.cameras.main.setRenderToTexture(this.invertPipeline);
         //this.add.image(400, 300, 'sky');
 
         //var particles = this.add.particles('red');
--- PhaserBase/pipelines.js
+++ PhaserBase/pipelines.js
@@ -14,14 +14,150 @@ var GrayscalePipeline = new Phaser.Class({
                 uniform sampler2D uMainSampler;
                 varying vec2 outTexCoord;
                 void main(void) {
-                //vec2 coord = vec2(outTexCoord.x, 1.0-outTexCoord.y);
-                vec4 color = texture2D(uMainSampler, outTexCoord);
-                float gray = dot(color.rgb, vec3(0.299, 0.587, 0.114));
-                float gray2 = (color.r + color.g + color.b) / 3.0;
-                vec2 unionCoord = outTexCoord - vec2(0.0,0.0);
-                float pct = sqrt(unionCoord.x * unionCoord.x + unionCoord.y * unionCoord.y);
-                gl_FragColor = vec4(vec3(gray2) * (1.0-pct), 1.0);
+                  vec4 color = texture2D(uMainSampler, outTexCoord);
+                  //float gray = dot(color.rgb, vec3(0.299, 0.587, 0.114));
+                  float gray2 = (color.r + color.g + color.b) / 3.0;
+                  gl_FragColor = vec4(vec3(gray2), 1.0);
                 }`
+        });
+    }
+});
+
+var InvertPipeline = new Phaser.Class({
+
+    Extends: Phaser.Renderer.WebGL.Pipelines.TextureTintPipeline,
+
+    initialize:
+
+    function InvertPipeline (game)
+    {
+        Phaser.Renderer.WebGL.Pipelines.TextureTintPipeline.call(this, {
+            game: game,
+            renderer: game.renderer,
+            fragShader:`
+                precision mediump float;
+                uniform sampler2D previusSampler;
+                varying vec2 outTexCoord;
+                void main(void) {
+                  vec4 color = texture2D(previusSampler, outTexCoord);
+                  gl_FragColor = vec4(1.0 - color.xyz, 1.0);
+                }`
+        });
+    }
+});
+
+var LightingPipeline = new Phaser.Class({
+  Extends: Phaser.Renderer.WebGL.Pipelines.TextureTintPipeline,
+
+  initialize:
+
+  function LightingPipeline (game)
+  {
+      Phaser.Renderer.WebGL.Pipelines.TextureTintPipeline.call(this, {
+          game: game,
+          renderer: game.renderer,
+          fragShader:`
+            precision mediump float;
+            uniform sampler2D uMainSampler;
+            //uniform sampler2D uOcclusionMap;
+            varying vec2 outTexCoord;
+
+            uniform vec2 res;
+            uniform vec2 light1_Pos;
+            uniform float pointLightInfo[6]; //pos.x, pos.y, weakness, color.x, color.y, color.z
+            uniform float focalLightInfo[9]; //pos.x, pos.y, dir.x, dir.y, angleRatio, weakness, color.x, color.y, color.z
+            uniform int numPointLights;
+            uniform int numFocalLights;
+
+            vec2 getPixelTexCoord(vec2 coord){
+              coord.x = floor(coord.x * res.x)/res.x + (1.0/(res.x*2.0));
+              coord.y = floor(coord.y * res.y)/res.y + (1.0/(res.y*2.0));
+              return coord;
+            }
+
+            vec3 getLightIntensity(vec2 coordDst){
+              vec3 color = vec3(1.0, 1.0, 0.5);
+              vec2 posSrc = vec2(light1_Pos.x, /*1.0 - */light1_Pos.y);
+              float lightWeakness = 3.0;
+
+              vec2 posSrcPixel = getPixelTexCoord(posSrc);
+              vec2 dir = coordDst - posSrcPixel;
+
+              float dstIntensity = 0.1;
+              dstIntensity += 1.0 - smoothstep(0.0, 1.0, length(dir) * lightWeakness);
+              vec3 colIntensity = dstIntensity * color;
+
+              return colIntensity;
+            }
+
+            /*vec3 calculateLighting(vec2 coordDst){
+
+            }*/
+
+            void main(void) {
+              vec2 outTexCoordFixed = vec2(outTexCoord.x, /*1.0 -*/ outTexCoord.y);
+              vec2 pixelCoord = getPixelTexCoord(outTexCoordFixed);
+              vec4 colorUnlit = texture2D(uMainSampler, pixelCoord);
+
+              vec4 colorLit = vec4(colorUnlit.xyz * getLightIntensity(pixelCoord), 1.0);
+              gl_FragColor = colorLit;
+              //gl_FragColor = vec4((vec3(color.w)-0.95) /0.05, 1.0);
+              //gl_FragColor = vec4(color.xyz, 1.0);
+              //gl_FragColor = texture2D(uMainSampler, outTexCoord);
+            }
+          `
+      });
+  }
+});
+
+var SurfaceCheckPipeline = new Phaser.Class({
+
+    Extends: Phaser.Renderer.WebGL.Pipelines.TextureTintPipeline,
+
+    initialize:
+
+    function SurfaceCheckPipeline (game)
+    {
+        Phaser.Renderer.WebGL.Pipelines.TextureTintPipeline.call(this, {
+            game: game,
+            renderer: game.renderer,
+            fragShader:`
+              precision mediump float;
+              uniform sampler2D uMainSampler;
+              varying vec2 outTexCoord;
+              void main(void) {
+                vec4 color = texture2D(uMainSampler, outTexCoord);
+                gl_FragColor = vec4(vec3(color.w), 1.0);
+                //gl_FragColor = vec4(1.0);
+              }`
+
+        });
+    }
+});
+
+var SurfacePipeline = new Phaser.Class({
+
+    Extends: Phaser.Renderer.WebGL.Pipelines.TextureTintPipeline,
+
+    initialize:
+
+    function SurfacePipeline (game)
+    {
+        Phaser.Renderer.WebGL.Pipelines.TextureTintPipeline.call(this, {
+            game: game,
+            renderer: game.renderer,
+            fragShader:`
+              precision mediump float;
+              uniform sampler2D uMainSampler;
+              uniform float lightTransfer;
+              varying vec2 outTexCoord;
+              void main(void) {
+                vec4 color = texture2D(uMainSampler, outTexCoord);
+                if(color.w == 0.0) discard;
+                gl_FragColor = vec4(vec3(lightTransfer), 1.0);
+
+                //gl_FragColor = vec4(1.0);
+              }`
         });
     }
 });
